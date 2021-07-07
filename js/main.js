2334ef70214167d7b06714f68dccce01e7362842
Taku Dev <72721832+Taku-freelance@users.noreply.github.com>
2021-07-08 00:01:09 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*
-* Select element function
+* ----------------- Select element function -------------------------- *
 */
 // (element) takes a whatever class such as list, .menu-toggle
 const selectElement = function (element) {
@@ -7,15 +7,66 @@ const selectElement = function (element) {
 };
 
 /*
-* Variable
+* ------------------ Variable ----------------------------------------- *
 */
 // The variable equal to the calling function
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
 /*
-* The event listener
+* ------------------ event Listener ----------------------------------- *
 */
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
+
+/*
+* ------------------ Scroll reveal ------------------------------------ *
+*/
+window.sr = ScrollReveal();
+
+// Select the enement
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '125rem',
+    delay: 300,
+    opacity: null,
+    reset: true
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '125rem',
+    delay: 600,
+    opacity: null,
+    reset: true
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '125rem',
+    delay: 600,
+    opacity: null,
+    reset: true
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '125rem',
+    delay: 600,
+    opacity: null,
+    reset: true 
+});
+
+
+
+function myCallback (el) {
+    el.classList.remove('is-animating');
+    el.classList.add('is-visible');
+}
+
+ScrollReveal().reveal('.animate-bottom', { afterReveal: myCallback });
